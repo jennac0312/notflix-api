e2f327b19a5e3abb80a9652c28d0a9009d68d69f
Jenna C <jennac0312@gmail.com>
2024-09-30 10:22:03 -0400

    browserrouter w basename

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppContextProvider from './contexts/app_context';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <AppContextProvider>
-        <Router>
+        <Router basename='/notflix-app'>
           <App />
         </Router>
       </AppContextProvider>
